776b5cb553243fad0389d36bfd9e12468108537a
ArthurZelo22 <arthur_t_souza@estudante.sesisenai.org.br>
2026-09-24 12:36:45 -0300

    Finalizada!

--- readme.sql
+++ readme.sql
@@ -67,7 +67,9 @@ INSERT INTO itens_matricula (matricula_id, modalidade_id, duracao_meses, valor_m
 (3, 3, 12, 90.00, 0.00),
 (4, 1, 1, 220.00, 50.00);
 
-------------------------------
+----------------------------
+
+Q1 
 
 CREATE VIEW vw_modalidades_custo_estimado AS
 SELECT 
@@ -79,6 +81,8 @@ FROM modalidades m
 JOIN planos p ON m.plano_id = p.id
 ORDER BY mensalidade_com_taxa DESC;
 
+Q2
+
 CREATE VIEW vw_matriculas_ativas AS
 SELECT 
     a.nome AS aluno, 
@@ -88,9 +92,45 @@ SELECT
     im.duracao_meses, 
     mat.data_inicio
 FROM matriculas mat
-JOIN alunos a ON mat.aluno_id = a.id
-JOIN itens_matricula im ON mat.id = im.matricula_id
-JOIN modalidades mod ON im.modalidade_id = mod.id
+
+JOIN alunos a 
+	ON mat.aluno_id = a.id
+JOIN itens_matricula im 
+	ON mat.id = im.matricula_id
+JOIN modalidades 
+	mod ON im.modalidade_id = mod.id
 WHERE mat.status = 'Ativa';
 
-SELECT * FROM vw_matriculas_ativas 
+-- Q3
+CREATE VIEW vw_alunos_vip AS
+SELECT 
+    a.nome AS aluno, 
+    COUNT(mat.id) AS total_matriculas, 
+    SUM((im.valor_mensal_aplicado * im.duracao_meses) + im.taxa_adesao) AS total_investido
+FROM alunos a
+JOIN matriculas mat ON a.id = mat.aluno_id
+JOIN itens_matricula im ON mat.id = im.matricula_id
+WHERE mat.status = 'Ativa'
+GROUP BY a.id, a.nome
+HAVING SUM((im.valor_mensal_aplicado * im.duracao_meses) + im.taxa_adesao) > 1000.00;
+
+-- Q4 
+SELECT m.*, p.nome AS plano, p.valor_mensal_base
+FROM modalidades m
+JOIN planos p ON m.plano_id = p.id
+WHERE m.capacidade_maxima >= 15 
+  AND p.valor_mensal_base > 100.00 
+  AND m.disponivel = TRUE;
+
+-- Q5
+CREATE VIEW vw_faturamento_medio_plano AS
+SELECT 
+    p.nome AS plano, 
+    SUM((im.valor_mensal_aplicado * im.duracao_meses) + im.taxa_adesao) AS faturamento_total,
+    ROUND(AVG(im.duracao_meses), 1) AS media_meses_contratados
+FROM itens_matricula im
+JOIN matriculas mat ON im.matricula_id = mat.id
+JOIN modalidades m ON im.modalidade_id = m.id
+JOIN planos p ON m.plano_id = p.id
+WHERE mat.status = 'Ativa'
+GROUP BY p.nome;
